104aa6d66e888d6baea6cd3c0770f94a038df348
Madeline <144378017+madelinestruthers@users.noreply.github.com>
2024-01-12 23:30:40 -0800

    Add files via upload

---
+++ components/Footer/index.js
@@ -0,0 +1,25 @@
+import styles from './Footer.module.css'
+
+export default function Footer(){
+    return(
+        <footer className={styles.footer}>
+          
+            <span>Meta</span>
+            <span>About</span>
+            <span> Blog </span>
+            <span>Job</span>
+            <span>Help</span>
+            <span>API</span>
+            <span>Privacy Terms</span>
+            <span> Locations</span>
+            <span> Instagram Lite</span>
+            <span> Threads</span>
+            <span>Contact Uploading & Non-Users</span>
+            <span>Meta Verified </span>
+            <br />
+           <span>English</span>
+            <span>© 2024 Instagram from Meta</span> 
+            
+        </footer>
+    )
+}
